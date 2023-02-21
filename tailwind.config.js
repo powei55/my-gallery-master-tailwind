c41ad0ae79572bd9f5dd9455a011837cb9d1603d
powel ikechukwu <ikechukwupowei21@gmail.com>
2023-02-21 17:36:30 +0100

    commiting gallery-master-page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      display: ["'Montserrat', sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
